1aeaa25876605f74a28572e9d06b360feb702d06
uchendu uchenna Daniel <uchenduuchenna7@gmail.com>
2026-03-05 14:35:20 +0100

    Update Home.jsx

--- CgpaCalculator/src/pages/Home/Home.jsx
+++ CgpaCalculator/src/pages/Home/Home.jsx
@@ -41,7 +41,7 @@ function FloatingSummaryButton({ onClick }) {
   useEffect(() => {
     const vw = window.innerWidth;
     const vh = window.innerHeight;
-    const initial = { x: vw - 80, y: vh - 160 };
+    const initial = { x: vw - 80, y: vh - 90 };
     pos.current = initial;
     setCoords(initial);
   }, []);
